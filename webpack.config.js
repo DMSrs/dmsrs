dd3fe25cd888ed6a383e34e6e9c805fce4aebeff
Andrea Faggin <blackbird5308@gmail.com>
2018-07-07 15:46:58 +0200

    Added config for WP

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: { main: './assets/javascripts/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'static', 'assets'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(path.resolve(__dirname, 'static', 'assets'), {}),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ]
+};
